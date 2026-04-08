93f9afe6a92923388a573c61e8b0f2ee35abdd79
Tornado6896 <tornado6896@gmail.com>
2026-04-08 23:17:32 +0300

    Update sync.h

--- include/android_stub/sync/sync.h
+++ include/android_stub/sync/sync.h
@@ -1,1 +1,15 @@
-../android/sync.h
+	#ifndef _ANDROID_SYNC_H
+	#define _ANDROID_SYNC_H
+
+	#ifdef __cplusplus
+	extern "C" {
+	#endif
+
+	int sync_wait(int fd, int timeout);
+	int sync_merge(const char* name, int fd1, int fd2);
+
+	#ifdef __cplusplus
+	}
+	#endif
+
+	#endif	
