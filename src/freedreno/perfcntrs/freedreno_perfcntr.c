56cc784d42200b43b9c67a024dc0172407055af1
Tornado6896 <tornado6896@gmail.com>
2026-04-09 22:45:05 +0300

    Update freedreno_perfcntr.c

--- src/freedreno/perfcntrs/freedreno_perfcntr.c
+++ src/freedreno/perfcntrs/freedreno_perfcntr.c
@@ -38,9 +38,6 @@ fd_perfcntrs(const struct fd_dev_id *id, unsigned *count)
    case 7:
       *count = a7xx_num_perfcntr_groups;
       return a7xx_perfcntr_groups;
-    case 8:
-     *count = a6xx_num_perfcntr_groups;
-     return a6xx_perfcntr_groups;
    default:
       *count = 0;
       return NULL;
@@ -57,9 +54,6 @@ fd_derived_counters(const struct fd_dev_id *id, unsigned *count)
    case 7:
       *count = a7xx_num_derived_counters;
       return a7xx_derived_counters;
-      case 8:
-      *count = a6xx_num_derived_counters;
-      return a6xx_derived_counters;
    default:
       *count = 0;
       return NULL;
@@ -76,9 +70,6 @@ fd_generate_derived_counter_collection(const struct fd_dev_id *id, struct fd_der
    switch (fd_dev_gen(id)) {
    case 7:
       a7xx_generate_derived_counter_collection(id, collection);
-      break;
-   case 8:
-      a6xx_generate_derived_counter_collection(id, collection);
       break;
    default:
       break;
