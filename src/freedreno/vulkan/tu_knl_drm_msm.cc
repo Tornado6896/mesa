71c6bdb022d538f279448d38479ecab2e449405f
Tornado6896 <tornado6896@gmail.com>
2026-04-09 22:54:55 +0300

    Update tu_knl_drm_msm.cc

--- src/freedreno/vulkan/tu_knl_drm_msm.cc
+++ src/freedreno/vulkan/tu_knl_drm_msm.cc
@@ -1667,7 +1667,7 @@ tu_knl_drm_msm_load(struct tu_instance *instance,
    device->has_sparse_prr = tu_drm_get_prr(device);
 
    device->has_preemption = tu_drm_has_preemption(device);
-
+   device->is_perf_cntr_selectable = true;
    /* Even if kernel is new enough, the GPU itself may not support it. */
    device->has_cached_coherent_memory =
       (device->msm_minor_version >= 8) &&
