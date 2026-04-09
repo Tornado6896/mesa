4e8f4f5b3602f308b512ab118e2a47b8c3cc60bd
Tornado6896 <tornado6896@gmail.com>
2026-04-09 21:59:47 +0300

    Update freedreno_perfcntr.c

--- src/freedreno/perfcntrs/freedreno_perfcntr.c
+++ src/freedreno/perfcntrs/freedreno_perfcntr.c
@@ -22,8 +22,8 @@ extern const unsigned a6xx_num_perfcntr_groups;
 extern const struct fd_perfcntr_group a7xx_perfcntr_groups[];
 extern const unsigned a7xx_num_perfcntr_groups;
 
- extern const struct fd_perfcntr_group a8xx_perfcntr_groups[];
- extern const unsigned a8xx_num_perfcntr_groups;
+// extern const struct fd_perfcntr_group a8xx_perfcntr_groups[];
+// extern const unsigned a8xx_num_perfcntr_groups;
 
 const struct fd_perfcntr_group *
 fd_perfcntrs(const struct fd_dev_id *id, unsigned *count)
@@ -42,7 +42,7 @@ fd_perfcntrs(const struct fd_dev_id *id, unsigned *count)
       *count = a7xx_num_perfcntr_groups;
       return a7xx_perfcntr_groups;
     case 8:
-     *count = a8xx_num_perfcntr_groups;
+     *count = a7xx_num_perfcntr_groups;
      return a7xx_perfcntr_groups;
    default:
       *count = 0;
@@ -60,8 +60,8 @@ fd_derived_counters(const struct fd_dev_id *id, unsigned *count)
    case 7:
       *count = a7xx_num_derived_counters;
       return a7xx_derived_counters;
-       case 8:
-       *count = a8xx_num_derived_counters;
+      case 8:
+      *count = a7xx_num_derived_counters;
       return a7xx_derived_counters;
    default:
       *count = 0;
@@ -95,7 +95,7 @@ fd_generate_derived_counter_collection(const struct fd_dev_id *id, struct fd_der
       a7xx_generate_derived_counter_collection(id, collection);
       break;
    case 8:
-      a8xx_generate_derived_counter_collection(id, collection);
+      a7xx_generate_derived_counter_collection(id, collection);
       break;
    default:
       break;
