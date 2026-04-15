384c8d4264c2a04fea21a27eba065c54ee36eb40
Tornado6896 <tornado6896@gmail.com>
2026-04-15 21:28:59 +0300

    Update fd6_hw.h

--- src/freedreno/common/fd6_hw.h
+++ src/freedreno/common/fd6_hw.h
@@ -16,6 +16,7 @@
 #include "a7xx_perfcntrs.xml.h"
 #include "a6xx_descriptors.xml.h"
 #include "a8xx_descriptors.xml.h"
+#include "a8xx_perfcntrs.xml.h"
 #include "a6xx.xml.h"
 
 #endif /* FD6_HW_H */
