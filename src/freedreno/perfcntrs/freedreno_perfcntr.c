9243513162713c425b84491f17a70f4477316c9b
Tornado6896 <tornado6896@gmail.com>
2026-04-15 23:48:49 +0300

    Update freedreno_perfcntr.c

--- src/freedreno/perfcntrs/freedreno_perfcntr.c
+++ src/freedreno/perfcntrs/freedreno_perfcntr.c
@@ -19,6 +19,10 @@ extern const unsigned a5xx_num_perfcntr_groups;
 extern const struct fd_perfcntr_group a6xx_perfcntr_groups[];
 extern const unsigned a6xx_num_perfcntr_groups;
 
+extern const struct fd_perfcntr_group a8xx_perfcntr_groups[];
+extern const unsigned a8xx_num_perfcntr_groups;
+
+
 extern const struct fd_perfcntr_group a7xx_perfcntr_groups[];
 extern const unsigned a7xx_num_perfcntr_groups;
 
@@ -38,6 +42,9 @@ fd_perfcntrs(const struct fd_dev_id *id, unsigned *count)
    case 7:
       *count = a7xx_num_perfcntr_groups;
       return a7xx_perfcntr_groups;
+   case 8:
+      *count = a8xx_num_perfcntr_groups;
+      return a8xx_perfcntr_groups;
    default:
       *count = 0;
       return NULL;
