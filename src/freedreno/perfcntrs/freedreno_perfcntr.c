cc3961e5c7f7fbb7a93d843a6165422edccd8f8c
Tornado6896 <tornado6896@gmail.com>
2026-04-16 00:56:14 +0300

    Add files via upload

--- src/freedreno/perfcntrs/freedreno_perfcntr.c
+++ src/freedreno/perfcntrs/freedreno_perfcntr.c
@@ -22,6 +22,9 @@ extern const unsigned a6xx_num_perfcntr_groups;
 extern const struct fd_perfcntr_group a7xx_perfcntr_groups[];
 extern const unsigned a7xx_num_perfcntr_groups;
 
+extern const struct fd_perfcntr_group a8xx_perfcntr_groups[];
+extern const unsigned a8xx_num_perfcntr_groups;
+
 const struct fd_perfcntr_group *
 fd_perfcntrs(const struct fd_dev_id *id, unsigned *count)
 {
@@ -38,6 +41,9 @@ fd_perfcntrs(const struct fd_dev_id *id, unsigned *count)
    case 7:
       *count = a7xx_num_perfcntr_groups;
       return a7xx_perfcntr_groups;
+   case 8:
+      *count = a8xx_num_perfcntr_groups;
+      return a8xx_perfcntr_groups;
    default:
       *count = 0;
       return NULL;
@@ -60,8 +66,6 @@ fd_derived_counters(const struct fd_dev_id *id, unsigned *count)
    }
 }
 
-
-
 extern void a7xx_generate_derived_counter_collection(const struct fd_dev_id *id, struct fd_derived_counter_collection *collection);
 
 void
@@ -75,4 +79,3 @@ fd_generate_derived_counter_collection(const struct fd_dev_id *id, struct fd_der
       break;
    }
 }
-
